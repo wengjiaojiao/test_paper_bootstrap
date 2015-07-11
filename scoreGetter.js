d50f88bf6ddfb7d90115c9b7d71b376991cd6f3e
wengjiaojiao <wengjiaojiao@outlook.com>
2015-07-11 17:23:21 +0800

    judge gap order and add textScore function

--- scoreGetter.js
+++ scoreGetter.js
@@ -1,3 +1,35 @@
-function scoreGetter() {
+var AllQuestionBank = require('./questionBank');
+var _ = require('./lodash.src.js');
 
+function scoreGetter(infors) {
+  var questionBanks = AllQuestionBank();
+  var sumScore = 0;
+
+  _.forEach(infors, function(infor, key) {
+    questionBanks.forEach(function(questionBank, i) {
+      if(questionBank.type == "texts") {
+        sumScore += textScore(infor, questionBank);
+      }else if(key == questionBank.name && infor == questionBank.answer) {
+        sumScore += questionBank.score;
+      }
+    });
+  });
+  return sumScore;
 }
+
+
+function textScore(infor, questionBank) {
+  var score = 0;
+  var answers = questionBank.answer;
+
+  _.forEach(infor,function(inf, i) {
+      console.log(inf);
+    answers.forEach(function(answer, i) {
+      if(inf == answer) {
+        score += questionBank.score;
+      }
+    })
+  })
+  return score;
+}
+module.exports = scoreGetter;
