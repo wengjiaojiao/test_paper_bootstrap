5dd9fd94793557e6ae09115b05eee7c339b2b2c9
wengjiaojiao <wengjiaojiao@outlook.com>
2015-07-11 17:22:03 +0800

    add type attribute

--- questionBank.js
+++ questionBank.js
@@ -2,15 +2,17 @@ var Question = require('./question');
 
 function AllQuestionBank() {
   return [
-    new Question("gapOne", "统一建模语言", 1),
-    new Question("gapTwo", "封装性继承性多态性", 3),
-    new Question("radioOne", "B", 2),
-    new Question("radioTwo", "A", 2),
-    new Question("checkOne", "ABD", 3),
-    new Question("checkTwo", "ABC", 3),
-    new Question("judgeOne", "F", 2),
-    new Question("judgeTwo", "T", 2),
-    new Question("textarea", "模型是对现实世界的简化和抽象。", 5)
+    new Question("gapOne", ["统一建模语言"], 1, "text"),
+    new Question("gapTwo", ["封装性"], 1, "texts"),
+    new Question("gapTwo", ["继承性"], 1, "texts"),
+    new Question("gapTwo", ["多态性"], 1, "texts"),
+    new Question("radioOne", "B", 2, "radio"),
+    new Question("radioTwo", "A", 2, "radio"),
+    new Question("checkOne", "ABD", 3, "checkbox"),
+    new Question("checkTwo", "ABC", 3, "checkbox"),
+    new Question("judgeOne", "F", 2, "judge"),
+    new Question("judgeTwo", "T", 2, "judge"),
+    new Question("textarea", "模型是对现实世界的简化和抽象。", 5, "textarea")
   ];
 }
 
