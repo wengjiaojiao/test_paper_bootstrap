be35087e9ca5e5af446cf73c8518222777e988ab
wengjiaojiao <wengjiaojiao@outlook.com>
2015-07-11 17:21:20 +0800

    add type attribut

--- question.js
+++ question.js
@@ -1,7 +1,8 @@
-function Question(name, answer, score) {
+function Question(name, answer, score, type) {
   this.name = name;
   this.answer = answer;
   this.score = score;
+  this.type = type;
 }
 
 module.exports = Question;
